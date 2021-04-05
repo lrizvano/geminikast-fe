154c407f57dd033837ec39d7ffd3dff72cb290ce
lrizvano <lis.rizvanolli@gmail.com>
2021-04-05 19:11:12 -0400

    update to pages and toml

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,16 +6,8 @@ export default function Home() {
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit <code>src/App.js</code> and save to reload.
+        Welcome to the Geminikast.
       </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Home
-      </a>
     </header>
   )
 }
--- src/components/podcast/Podcast.js
+++ src/components/podcast/Podcast.js
@@ -3,12 +3,6 @@ import logo from '../../geminikast.jpg';
 
 export default function Podcast() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <iframe src="https://open.spotify.com/embed-podcast/show/77QfKMGg067YXiJnb1Ic3q" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-    </header>
+    <iframe src="https://open.spotify.com/embed-podcast/show/77QfKMGg067YXiJnb1Ic3q" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   )
 }

1583b8e6eb3732d16ea3f0d977046184fe85da52
lrizvano <lis.rizvanolli@gmail.com>
2021-04-05 11:48:42 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
+import logo from './geminikast.jpg';
 import './App.css';
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const renderNavbar = () => (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />
+        {' Geminikast'}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#link">Podcast</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+
   return (
     <div className="App">
+      {renderNavbar()}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

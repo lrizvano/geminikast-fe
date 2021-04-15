e8785ac16879c6a253e12803d04a0b8dd3ad02ff
lrizvano <lis.rizvanolli@gmail.com>
2021-04-15 11:02:34 -0400

    replace Link with Nav.Link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,29 +2,26 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import logo from '../../geminikast-logo.jpg'
-import { Link } from 'react-router-dom'
 
 export default function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand>
-        <Link to="/">
-          <img
-            alt=""
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />
-          {'Geminikast'}
-        </Link>
+      <Navbar.Brand href='/'>
+        <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        Geminikast
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Link to="/news">News</Link>
-          <Link to="/reviews">Reviews</Link>
-          <Link to="/about">About</Link>
+          <Nav.Link href="/news">News</Nav.Link>
+          <Nav.Link href="/reviews">Reviews</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>

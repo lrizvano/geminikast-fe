e7db903280728493436e6543f9d2441da2f2d8fd
lrizvano <lis.rizvanolli@gmail.com>
2021-04-15 21:10:17 -0400

    fix footer alignment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 import React from 'react'
 import './custom.scss'
 import Navigation from './components/Navigation.js'
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Layout from './Layout.js'
+
+const links = [
+  "https://twitter.com/geminikatz",
+  "https://open.spotify.com/show/77QfKMGg067YXiJnb1Ic3q",
+  "https://www.youtube.com/channel/UCG6B9yuk_K4MITArK719LUw",
+  "https://www.instagram.com/thegeminikast",
+  "https://www.twitch.tv/geminikatz",
+]
 
 export default function Footer() {
+  const renderLinks = () => {
+      return (
+        links.map(link => (
+          <Col xs={1}>
+            <SocialIcon url={`${link}`}/>
+          </Col>
+        ))
+      )
+  }
+
   return (
-    <div>
-      <SocialIcon url="https://twitter.com/geminikatz"/>
-      <SocialIcon url="https://open.spotify.com/show/77QfKMGg067YXiJnb1Ic3q"/>
-      <SocialIcon url="https://www.youtube.com/channel/UCG6B9yuk_K4MITArK719LUw"/>
-      <SocialIcon url="https://www.instagram.com/thegeminikast"/>
-      <SocialIcon url="https://www.twitch.tv/geminikatz"/>
-    </div>
+    <Layout>
+      <Row className="justify-content-md-center">
+        {renderLinks()}
+      </Row>
+    </Layout>
   )
 }

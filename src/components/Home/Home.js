fa286a588f8a7fa4e2dae42812647153d3645807
lrizvano <lis.rizvanolli@gmail.com>
2021-04-19 21:19:10 -0400

    draft home page text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Podcast from "./Podcast.js";
 import Featured from "./Featured.js";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin-top: 1rem;
+  color: var(--primary);
+`;
 
 export default function Home() {
   return (
     <>
+      <Title>Recent Updates</Title>
       <Featured />
+
+      <Title>Newest Episode</Title>
       <Podcast />
     </>
   );

4c1f4d9abf277e153f614e53cb354c07c41b67ac
lrizvano <lis.rizvanolli@gmail.com>
2021-04-29 19:09:30 -0400

    polish author view styling

--- src/components/About/AuthorView/AuthorCover.js
+++ src/components/About/AuthorView/AuthorCover.js
@@ -4,23 +4,32 @@ import { client } from "../../../prismic-configuration.js";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 export default function ArticleCover(props) {
   return (
     <>
-      <Row>
+      <Row className="mb-3">
         <Col xs="auto">
           <Image src={props.image} />
         </Col>
-        <Col xs="auto">
-          <h1 className="mt-3 text-primary">{props.name}</h1>
-          <small className="text-muted">{props.role}</small>
-          <RichText
-            render={props.bio}
-            htmlSerializer={client.htmlSerializer}
-          ></RichText>
-        </Col>
+        <Wrapper>
+          <Col xs="auto">
+            <h1 className="mt-3 text-primary">{props.name}</h1>
+            <small className="text-muted">{props.role}</small>
+          </Col>
+        </Wrapper>
       </Row>
+      <RichText
+        render={props.bio}
+        htmlSerializer={client.htmlSerializer}
+      ></RichText>
     </>
   );
 }

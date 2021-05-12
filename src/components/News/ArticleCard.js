df8cfd3951e6b0c5c0655144c3e818ad8bfd35ae
lrizvano <lis.rizvanolli@gmail.com>
2021-05-11 22:56:55 -0400

    copy review card styling to article card

--- src/components/News/ArticleCard.js
+++ src/components/News/ArticleCard.js
@@ -1,19 +1,52 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import styled from "styled-components";
+
+const Hover = styled.section`
+  #card {
+    position: relative;
+    overflow: hidden;
+    border-color: var(--dark);
+    #text {
+      position: absolute;
+      bottom: 0px;
+      color: var(--light);
+      width: 100%;
+      background: linear-gradient(hsl(0 0% 0% / 0), hsl(0 0% 0% / 1));
+    }
+
+    @media (hover) {
+      #text {
+        transform: translateY(35%);
+        transition: transform 500ms ease;
+      }
+      &:hover,
+      &:focus-within {
+        border-color: var(--primary);
+        #text {
+          color: var(--primary);
+          transform: translateY(0%);
+        }
+      }
+    }
+  }
+`;
 
 export default function ArticleCard(props) {
   return (
     <Col xs="12" sm="12" md="6" lg="4" xl="4">
-      <Card bg="primary" className="mb-4">
-        <Card.Link href={`news/${props.uid}`} className="text-dark">
-          <Card.Img variant="top" src={props.image} />
-          <Card.Body>
-            <Card.Title>{props.headline}</Card.Title>
-            <small className="text-muted">By {props.author}</small>
-          </Card.Body>
-        </Card.Link>
-      </Card>
+      <Hover>
+        <Card id="card" className="mb-4">
+          <Card.Link href={`reviews/${props.uid}`}>
+            <Card.Img variant="top" src={props.image} />
+            <Card.Body id="text">
+              <Card.Title>{props.headline}</Card.Title>
+              <small className="text-muted">By {props.author}</small>
+            </Card.Body>
+          </Card.Link>
+        </Card>
+      </Hover>
     </Col>
   );
 }

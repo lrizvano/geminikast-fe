37558cb4f5b411d475dfa14f1d323b8ac524fc1f
lrizvano <lis.rizvanolli@gmail.com>
2021-05-12 17:20:39 -0400

    add banner to review score card

--- src/components/Reviews/ReviewView/ReviewBody.js
+++ src/components/Reviews/ReviewView/ReviewBody.js
@@ -5,6 +5,19 @@ import Badge from "react-bootstrap/Badge";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import styled from "styled-components";
+
+const Score = styled.section`
+  #banner {
+    position: absolute;
+    top: 0;
+    left: 2%;
+    color: var(--primary);
+    background-color: var(--dark);
+    padding-bottom: 0.5em;
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 50% 75%, 0 100%);
+  }
+`;
 
 export default function ReviewBody(props) {
   return (
@@ -13,16 +26,18 @@ export default function ReviewBody(props) {
         render={props.body}
         htmlSerializer={client.htmlSerializer}
       ></RichText>
-      <Card className="bg-primary text-dark p-3 mb-3">
-        <Row>
-          <Col xs="auto">
-            <h1>
-              <Badge variant="primary text-dark">{props.score / 10}</Badge>
-            </h1>
-          </Col>
-          <Col>{props.summary}</Col>
-        </Row>
-      </Card>
+      <Score>
+        <Card className="bg-primary text-dark p-3 mb-3">
+          <Row>
+            <Col md={{ span: 11, offset: 1 }} xs={{ span: 10, offset: 2 }}>
+              {props.summary}
+            </Col>
+            <h2 id="banner">
+              <Badge>{props.score / 10}</Badge>
+            </h2>
+          </Row>
+        </Card>
+      </Score>
     </>
   );
 }

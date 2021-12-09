4302b878ef1b68394fd97df60742af29817e20ba
lrizvano <lis.rizvanolli@gmail.com>
2021-12-08 23:18:03 -0500

    Remove carousel from home page

--- src/components/Home/Features.js
+++ src/components/Home/Features.js
@@ -1,86 +1,32 @@
 import React from "react";
-import Prismic from "@prismicio/client";
 import { RichText } from "prismic-reactjs";
-import { client } from "../../prismic-configuration.js";
-import Carousel from "react-bootstrap/Carousel";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import ContentCard from "../Content/ContentCard.js";
+import Row from "react-bootstrap/Row";
 
-const Icons = styled.section`
-  margin: 0 3rem 0 3rem;
-
-  .carousel-inner {
-    border-radius: 12px 12px 0 0;
-  }
-  .carousel-control-prev-icon {
-    width: 40px;
-    height: 40px;
-    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='cyan' width='8' height='8' viewBox='0 0 8 8'%3e%3cpath d='M5.25 0l-4 4 4 4 1.5-1.5L4.25 4l2.5-2.5L5.25 0z'/%3e%3c/svg%3e");
-  }
-  .carousel-control-next-icon {
-    width: 40px;
-    height: 40px;
-    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='cyan' width='8' height='8' viewBox='0 0 8 8'%3e%3cpath d='M2.75 0l-1.5 1.5L3.75 4l-2.5 2.5L2.75 8l4-4-4-4z'/%3e%3c/svg%3e");
-  }
-  .carousel-indicators li {
-    width: 60px;
-    height: 6px;
-    background-color: var(--light);
-    margin-bottom: 15px;
-  }
-  .carousel-caption {
-    position: relative;
-    left: 0;
-    margin-top: 20px;
-    color: var(--light);
-    background-color: var(--secondary);
-    border-radius: 0 0 12px 12px;
-    z-index: 0;
-  }
-`;
-
-export default function Features() {
-  const [docs, setDocs] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const response = await client.query(
-        Prismic.Predicates.any("document.type", ["article", "review"]),
-        { orderings: "[document.first_publication_date desc]" }
-      );
-      if (response) {
-        setDocs(response.results);
-      }
-    };
-    fetchData();
-  }, []);
-
-  const renderDocs = () => {
-    return docs.map((doc) => (
-      <Carousel.Item key={doc.uid}>
-        <Link
-          to={doc.type === "article" ? `news/${doc.uid}` : `reviews/${doc.uid}`}
-        >
-          <img className="d-block w-100" src={doc.data.image.url} alt="" />
-          <Carousel.Caption>
-            <h3 class="font-weight-bold">
-              {doc.type === "article"
-                ? RichText.asText(doc.data.headline)
-                : RichText.asText(doc.data.game)}
-            </h3>
-            <p>{RichText.asText(doc.type)}</p>
-          </Carousel.Caption>
-        </Link>
-      </Carousel.Item>
-    ));
+export default function Features(props) {
+  const renderFeatures = () => {
+    console.log(props.features);
+    return props.features.map((doc) => {
+      const contentCardData = {
+        link: doc.type === "article" ? `news/${doc.uid}` : `reviews/${doc.uid}`,
+        image: doc.data.image.url || doc.data.image.url,
+        title:
+          doc.type === "article"
+            ? RichText.asText(doc.data.headline)
+            : RichText.asText(doc.data.game),
+        text: RichText.asText(doc.data.author.data.name),
+      };
+      return <ContentCard {...contentCardData} />;
+    });
   };
 
   return (
     <>
-      <h3 className="text-primary">Newest Features</h3>
-      <Icons>
-        <Carousel>{renderDocs()}</Carousel>
-      </Icons>
+      <hr className="bg-primary ml-3 mr-3 mt-5 mb-5" />
+      <h3 className="text-primary">
+        {props.page === "news" ? "Newest Articles" : "Newest Reviews"}
+      </h3>
+      <Row>{renderFeatures()}</Row>
     </>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,38 @@
 import React from "react";
 import Podcast from "./Podcast.js";
 import Features from "./Features.js";
+import Prismic from "@prismicio/client";
+import { client } from "../../prismic-configuration.js";
 
 export default function Home() {
+  const [reviews, setReviews] = React.useState([]);
+  const [articles, setArticles] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const reviewDocs = await client.query(
+        Prismic.Predicates.at("document.type", "review"),
+        { fetchLinks: "author.name", orderings: "[my.review.date desc]" }
+      );
+      const articleDocs = await client.query(
+        Prismic.Predicates.at("document.type", "article"),
+        { fetchLinks: "author.name", orderings: "[my.article.date desc]" }
+      );
+      if (reviewDocs) {
+        setReviews(reviewDocs.results);
+      }
+      if (articleDocs) {
+        setArticles(articleDocs.results);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Podcast />
-      <hr className="bg-primary ml-3 mr-3 mt-5 mb-5" />
-      <Features className="ml-3 mr-3" />
+      <Features page={"news"} features={articles} />
+      <Features page={"reviews"} features={reviews} />
     </>
   );
 }

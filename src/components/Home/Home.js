391c3d8d44094dd678ba8fd0e2bf41af307e98fc
lrizvano <lis.rizvanolli@gmail.com>
2021-12-08 22:28:07 -0500

    Pull podcast widget to top of home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,9 @@ import Features from "./Features.js";
 export default function Home() {
   return (
     <>
-      <Features className="ml-3 mr-3" />
-      <hr className="bg-primary ml-3 mr-3 mt-5 mb-5" />
       <Podcast />
+      <hr className="bg-primary ml-3 mr-3 mt-5 mb-5" />
+      <Features className="ml-3 mr-3" />
     </>
   );
 }

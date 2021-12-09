6a6c2e948632eabc41b39ceb8bdaf35064c97b4e
lrizvano <lis.rizvanolli@gmail.com>
2021-12-08 22:27:04 -0500

    Move jumbotron to about page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,10 @@
 import React from "react";
 import Podcast from "./Podcast.js";
 import Features from "./Features.js";
-import Jumbotron from "react-bootstrap/Jumbotron";
 
 export default function Home() {
   return (
     <>
-      <Jumbotron className="mt-3 bg-primary text-dark">
-        <h1>The Geminikast</h1>
-        <h2 className="text-muted">Games, Tech, and Movie Discussions!</h2>
-      </Jumbotron>
-      <hr className="bg-primary ml-3 mr-3 mt-5 mb-5" />
       <Features className="ml-3 mr-3" />
       <hr className="bg-primary ml-3 mr-3 mt-5 mb-5" />
       <Podcast />

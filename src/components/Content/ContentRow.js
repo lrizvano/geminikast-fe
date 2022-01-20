99e3dd873466af59bb8806e50f2b633e5c026e67
lrizvano <lis.rizvanolli@gmail.com>
2022-01-20 10:23:42 -0500

    Remove padding from content row columns

--- src/components/Content/ContentRow.js
+++ src/components/Content/ContentRow.js
@@ -3,31 +3,43 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Hover from "../Hover";
+import styled from "styled-components";
+
+const Padding = styled.section`
+  .col-6 {
+    padding-right: 0;
+  }
+  .col-6:nth-child(2) {
+    padding-left: 0;
+  }
+`;
 
 export default function ContentCard(props) {
   return (
     <Hover>
       <Card className="mb-3">
         <Card.Link href={props.link}>
-          <Row>
-            <Col xs="6" md="4" lg="3">
-              <Card.Img src={props.image} />
-            </Col>
-            <Col xs="6">
-              <Card.Body>
-                <Card.Title className="font-weight-bold">
-                  {props.title}
-                </Card.Title>
-                <Card.Subtitle className="mb-2">
-                  {props.author}{" "}
-                  {props.link.startsWith("news")
-                    ? "reported on"
-                    : "reviewed on"}{" "}
-                  {props.date}
-                </Card.Subtitle>
-              </Card.Body>
-            </Col>
-          </Row>
+          <Padding>
+            <Row>
+              <Col xs="6" md="4" lg="3">
+                <Card.Img src={props.image} />
+              </Col>
+              <Col xs="6">
+                <Card.Body>
+                  <Card.Title className="font-weight-bold">
+                    {props.title}
+                  </Card.Title>
+                  <Card.Subtitle className="mb-2">
+                    {props.author}{" "}
+                    {props.link.startsWith("news")
+                      ? "reported on"
+                      : "reviewed on"}{" "}
+                    {props.date}
+                  </Card.Subtitle>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Padding>
         </Card.Link>
       </Card>
     </Hover>

f1b9bb127af8358f3c46b80a313eb3e111be4a00
lrizvano <lis.rizvanolli@gmail.com>
2022-01-26 17:41:18 -0500

    Add filter functionality to review list

--- src/components/Reviews/ReviewList.js
+++ src/components/Reviews/ReviewList.js
@@ -4,14 +4,22 @@ import { client } from "../../prismic-configuration.js";
 import ContentRow from "../ContentRow.js";
 import { RichText, Date } from "prismic-reactjs";
 import DateFormat from "../DateFormat.js";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+
+const platformList = ["PC", "Playstation", "Xbox", "Movie", "Tabletop"];
 
 export default function ReviewList() {
   const [reviews, setReviews] = React.useState([]);
+  const [platforms, setPlatforms] = React.useState([]);
 
   React.useEffect(() => {
     const fetchData = async () => {
       const response = await client.query(
-        Prismic.Predicates.at("document.type", "review"),
+        [
+          Prismic.Predicates.at("document.type", "review"),
+          Prismic.Predicates.any("my.review.platforms.platform", platforms),
+        ],
         { fetchLinks: "author.name", orderings: "[my.review.date desc]" }
       );
       if (response) {
@@ -19,7 +27,7 @@ export default function ReviewList() {
       }
     };
     fetchData();
-  }, []);
+  }, [platforms]);
 
   const renderReviews = () => {
     return reviews.map((review) => {
@@ -36,8 +44,17 @@ export default function ReviewList() {
 
   return (
     <>
-      <h1 className="mt-5 mb-3 text-primary">All Reviews</h1>
-      {renderReviews()}
+      <h1 className="mt-5 mb-3 text-primary">
+        {platforms.length === 0 ? "All Reviews" : "Filtered Reviews"}
+      </h1>
+      <ToggleButtonGroup type="checkbox" onChange={setPlatforms}>
+        {platformList.map((platformName) => (
+          <ToggleButton variant="dark" value={platformName}>
+            {platformName}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+      {reviews.length === 0 ? <p>No filtered reviews</p> : renderReviews()}
     </>
   );
 }

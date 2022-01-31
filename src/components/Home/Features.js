b906213cd231ad8d9743c40b88830bffc3aaff2b
lrizvano <lis.rizvanolli@gmail.com>
2022-01-31 14:38:23 -0500

    Add jumbotrons to home page

--- src/components/Home/Features.js
+++ src/components/Home/Features.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TileCard from "../templates/TileCard.js";
 import Row from "react-bootstrap/Row";
+import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
 import styled from "styled-components";
 import { formatTileData } from "../../utils/formatters.js";
 import { documentTypes, featureDocuments } from "../../utils/queries";
+import { sortList } from "../../utils/filters.js";
 
 const Wrapper = styled.section`
   display: flex;
@@ -29,9 +31,13 @@ export default function Features(props) {
 
   return (
     <>
-      <h3 className="text-primary mb-3">
-        Latest {documentTypes[props.type].name}
-      </h3>
+      <Jumbotron className="bg-secondary text-info">
+        <h1 className="font-weight-bold">
+          {sortList[props.type][Object.keys(sortList[props.type])[0]].title}{" "}
+          {documentTypes[props.type].name}
+        </h1>
+        <h2>{documentTypes[props.type].description}</h2>
+      </Jumbotron>
       <Row>{renderFeatures()}</Row>
       <Wrapper>
         <Button
